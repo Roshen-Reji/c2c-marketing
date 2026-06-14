4e3e51596cf196a7767ef4d8274bc5041572d730
Roshen-Reji <roshenreji09@gmail.com>
2026-06-14 19:22:13 +0530

    chore: update basePath in next.config.ts to support wildcard routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/',
+  basePath: '/*',
 
   serverExternalPackages: ["firebase-admin", "jose", "jwks-rsa"],
 };
