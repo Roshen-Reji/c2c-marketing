51a7a58001da5c533024a612733ede6bfc7a1f56
Roshen-Reji <roshenreji09@gmail.com>
2026-06-14 19:12:34 +0530

    fix: update basePath to root path in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/c2c',
+  basePath: '/',
 
   serverExternalPackages: ["firebase-admin", "jose", "jwks-rsa"],
 };
